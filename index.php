727a7b282eeb557142e2197e72b75df096744565
OpenShift guest <chris.sgriffin@gmail.com>
2013-07-08 19:13:07 -0400

    Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+global $SETTINGS;
+$SETTINGS->pageTitle = "Home";
+$SETTINGS->pageCSS[] = 'home';
+
+include('partials/header.php');
+?>
+    
+<!-- Add your site or application content here -->
+<p>Hello world! This is me, myself, and I.</p>
+
+
+<?php include('partials/scripts.inc') ?>
+
+</body>
+</html>
